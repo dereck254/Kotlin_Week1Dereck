989fab1094774072bdacfdd019973ff7f955a26c
PC <dereckongaro1086@gmail.com>
2023-04-28 11:26:56 +0300

    ALU in kotlin

---
+++ src/main/kotlin/SimpleCalc.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    println("---Enter num1---")
+    var num1 = readLine()!!.toInt()
+    println("---Enter num2---")
+    var num2 = readLine()!!.toInt()
+    println("Sum=num1+num2")
+    var Sum = num1 + num2
+    println(Sum)
+
+    println("---Enter num3---")
+    var num3 = readLine()!!.toInt()
+    println("---Enter num4---")
+    var num4 = readLine()!!.toInt()
+    println("Product=num3*num4")
+    var Product = num3 * num4
+    println(Product)
+
+    println("---Enter num5---")
+    var num5 = readLine()!!.toInt()
+    println("---Enter num6---")
+    var num6 = readLine()!!.toInt()
+    println("Difference=num5-num6")
+    var Difference = num5 - num6
+    println(Difference)
+
+    println("---Enter num7---")
+    var num7 = readLine()!!.toInt()
+    println("---Enter num8---")
+    var num8 = readLine()!!.toInt()
+    println("Division=num7/num8")
+    var Division = num7 / num8
+    println(Division)
+
+}
