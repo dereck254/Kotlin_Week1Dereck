3a8861b3ec36f05a36ed6964ab69e095cbee45cd
PC <dereckongaro1086@gmail.com>
2023-04-28 10:51:58 +0300

    dowhilechallenge

--- src/main/kotlin/DoWhileChallenge.kt
+++ src/main/kotlin/DoWhileChallenge.kt
@@ -1,14 +1,14 @@
-//fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 
 
-    // var pin:Int
-    //do {
-        //println("Enter You Pin")
-       // pin = readLine()!!.toInt()
+    var pin:Int
+    do {
+        println("Enter You Pin")
+        pin = readLine()!!.toInt()
         
-    //} while (pin != 1220)
+    } while (pin != 1220)
 
-    //println("Correct Pin")
+    println("Correct Pin")
 
 
-//}
+}
